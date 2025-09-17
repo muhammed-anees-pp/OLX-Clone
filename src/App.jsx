cf46ba96131da62635c832d7fa9bbeb2afb6da9b
muhammed-anees-pp <muhammedanees.work@gmail.com>
2025-09-17 12:05:03 +0530

    feat: add product cards component with sample data and grid styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Login from './Pages/Login/Login'
 import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
 import Categories from './Components/CategoryTab/Categories'
+import Cards from './Components/Cards/Cards'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Categories/>
+      <Cards/>
       <Footer/>
 
     </div>
